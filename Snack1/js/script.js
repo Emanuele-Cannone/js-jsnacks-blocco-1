b8209f399908a07ebcc20d727ddcbdbf9b5534dd
Emanuele <cannone.emanuele@gmail.com>
2020-12-18 10:42:12 +0100

    Init

---
+++ Snack1/js/script.js
@@ -0,0 +1,27 @@
+// Primo snack
+// Il software deve chiedere per 5 volte all’utente di inserire un numero. 
+// Il programma stampa la somma di tutti i numeri inseriti. 
+// Esegui questo programma in due versioni, con il for e con il while
+
+// var sommaNumeri = 0;
+
+// for (var i = 0; i < 5; i++) {
+//   var numeroUtente = parseInt(prompt('inserisci un numero'));
+//   sommaNumeri = sommaNumeri + numeroUtente;
+// }
+
+
+// LA RICHIESTA DI QUALCOSA DEVE ESSERE INSERITA NEL FOR SE VOGLIAMO CHE SI RIPETA!!!!!
+
+
+// console.log('la somma totale è ', sommaNumeri);
+
+
+  // var i = 0; IL VALORE INIZIALE DELLA i DEVE SSERE SEMPRE ESPRESSA
+  // while (i < 5){
+  //   var numeroUtente = parseInt(prompt('inserisci un numero'));
+  //   i++;
+  //   sommaNumeri += numeroUtente;
+  // }
+
+  // console.log(sommaNumeri);
---
+++ Snack2/js/script.js
@@ -0,0 +1,4 @@
+// Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, 
+// se è dispari inseriscilo nell’array (modificato) 
+
+
